b9d44c9ea55558f34bdc9c8298a9d6b0f5faeae8
coalchan <coalchan@gmail.com>
2019-07-05 18:04:01 +0800

    update(spark)(streaming): modify comments

--- spark-study/streaming/src/main/scala/com/luckypeng/study/spark/streaming/HelloWorld.scala
+++ spark-study/streaming/src/main/scala/com/luckypeng/study/spark/streaming/HelloWorld.scala
@@ -11,6 +11,8 @@ object HelloWorld {
   def main(args: Array[String]): Unit = {
     // Create a local StreamingContext with two working thread and batch interval of 1 second.
     // The master requires 2 cores to prevent a starvation scenario.
+    // 注意，这里用的是 local[2]，本地运行时一个线程将用于接收数据，一个线程用于处理数据，要保证的是线程数要大于 Receiver 个数
+    // 详情请参考 http://spark.apache.org/docs/latest/streaming-programming-guide.html#input-dstreams-and-receivers
     val conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount")
     val ssc = new StreamingContext(conf, Seconds(1))
 
