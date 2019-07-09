a4ac0122b931fdce9ba574ae8ca4715139f333de
coalchan <coalchan@gmail.com>
2019-07-09 10:15:29 +0800

    add(spark)(streaming): stateful transformation: mapWithState

--- spark-study/streaming/src/main/scala/com/luckypeng/study/spark/streaming/state/StateOperations.scala
+++ spark-study/streaming/src/main/scala/com/luckypeng/study/spark/streaming/state/StateOperations.scala
@@ -1,7 +1,7 @@
 package com.luckypeng.study.spark.streaming.state
 
 import org.apache.spark.SparkConf
-import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming._
 import org.junit.jupiter.api.Test
 
 class StateOperations {
@@ -44,4 +44,45 @@ class StateOperations {
     ssc.awaitTermination()
   }
 
+  /**
+    * mapWithState
+    */
+  @Test
+  def testMapWithState: Unit = {
+    val conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount")
+    val ssc = new StreamingContext(conf, Seconds(5))
+
+    // mapWithState 操作需要 checkpoint
+    ssc.checkpoint("checkpoint-dir")
+
+    val lines = ssc.socketTextStream("localhost", 9999)
+
+    val wordsCounts = lines
+      .flatMap(_.split(" "))
+      .map(word => (word, 1))
+
+    // 3 个参数分别为 key, value, state
+    val func = (word: String, count: Option[Int], state: State[Int]) => {
+      println("key: " + word + ", count: " + count)
+      if (state.isTimingOut()) {
+        println(word + " is timing out: " + state.get())
+      }
+      else {
+        val sum = count.getOrElse(0) + (if (state.exists()) state.get() else 0)
+        state.update(sum)
+        (word, count)
+      }
+    }
+
+    val result = wordsCounts.mapWithState(StateSpec.function(func).timeout(Seconds(10)))
+
+    // 打印当前值
+    result.print()
+
+    // 获取全部 state 的值，并打印
+    result.stateSnapshots().print()
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
 }
