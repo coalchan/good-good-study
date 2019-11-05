94a7c97e3504085bd0a5b1075b9807e68e93688d
coalchan <coalchan@gmail.com>
2019-11-05 22:16:27 +0800

    add(hikaricp)(hello-world): init

---
+++ hikaricp-study/hello-world/src/main/java/com/luckypeng/study/hikaricp/hello/Test.java
@@ -0,0 +1,11 @@
+package com.luckypeng.study.hikaricp.hello;
+
+/**
+ * @author coalchan
+ * @date 2019/11/05
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello-world");
+    }
+}
