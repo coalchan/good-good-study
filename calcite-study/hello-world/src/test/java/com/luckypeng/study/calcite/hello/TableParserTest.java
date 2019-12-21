9ce06287b2612e5cb4702bb080720ce4ff511cd9
coalchan <coalchan@gmail.com>
2019-12-21 16:13:31 +0800

    add(calcite): hello world

---
+++ calcite-study/hello-world/src/test/java/com/luckypeng/study/calcite/hello/TableParserTest.java
@@ -0,0 +1,48 @@
+package com.luckypeng.study.calcite.hello;
+
+import com.google.common.collect.Sets;
+import org.apache.calcite.avatica.util.Casing;
+import org.apache.calcite.schema.SchemaPlus;
+import org.apache.calcite.sql.SqlNode;
+import org.apache.calcite.sql.parser.SqlParseException;
+import org.apache.calcite.sql.parser.SqlParser;
+import org.apache.calcite.sql.parser.impl.SqlParserImpl;
+import org.apache.calcite.sql.validate.SqlConformanceEnum;
+import org.apache.calcite.tools.FrameworkConfig;
+import org.apache.calcite.tools.Frameworks;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+public class TableParserTest {
+    private static SqlParser.Config config;
+
+    @Before
+    public void setUp() {
+        SchemaPlus rootSchema = Frameworks.createRootSchema(true);
+        final FrameworkConfig frameworkConfig =
+                Frameworks.newConfigBuilder()
+                        .defaultSchema(rootSchema)
+                        .parserConfig(
+                                SqlParser.configBuilder()
+                                        .setParserFactory(SqlParserImpl.FACTORY)
+                                        .setCaseSensitive(false)
+                                        .setQuotedCasing(Casing.TO_LOWER)
+                                        .setUnquotedCasing(Casing.TO_LOWER)
+                                        .setConformance(SqlConformanceEnum.MYSQL_5)
+                                        .build()
+                        ).build();
+        config = frameworkConfig.getParserConfig();
+    }
+
+    @Test
+    public void extractTableInSelect() throws SqlParseException {
+        SqlParser parser = SqlParser.create(Sql.SELECT_SQL, config);
+        SqlNode sqlNode = parser.parseStmt();
+        Set<String> tables = TableParser.extractTableInSelect(sqlNode);
+        assertEquals(Sets.newHashSet("dwd.customer", "dws.orders", "nation"), tables);
+    }
+}
