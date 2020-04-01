f6ef49a06680313c78eac9569802bb02eec540c2
coalchan <coalchan@gmail.com>
2020-04-01 10:18:30 +0800

    update(thrift)(impala): 修改单元测试

--- thrift-study/hiveserver2/src/test/java/com/luckypeng/study/thrift/hs2/ImpalaHS2ClientTest.java
+++ thrift-study/hiveserver2/src/test/java/com/luckypeng/study/thrift/hs2/ImpalaHS2ClientTest.java
@@ -21,15 +21,14 @@ public class ImpalaHS2ClientTest {
 
     @Test
     public void testAsync() throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Guid: ");
-        String guid = scanner.nextLine();
-        System.out.println("Secret: ");
-        String secret = scanner.nextLine();
-
         ImpalaHS2Client impalaHS2Client =
                 new ImpalaHS2Client("kuber02", 21050, 60,"thrift-test", "");
         ImpalaHS2Client.Hiverserver2 hiverserver2 = impalaHS2Client.getClient();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Guid: ");
+        String guid = scanner.nextLine();
+        String secret = guid;
 
         THandleIdentifier operationId = new HandleIdentifier(UUID.fromString(guid), UUID.fromString(secret))
                 .toTHandleIdentifier();
